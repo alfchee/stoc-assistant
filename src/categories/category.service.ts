ccb75fbfa536baae548747cb39c4b165041a9d31
Luis A Chee <alfchee@gmail.com>
2021-06-29 14:51:24 -0600

    Adding more logs:

--- src/categories/category.service.ts
+++ src/categories/category.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Category, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CategoryService {
+  private readonly logger = new Logger(CategoryService.name);
+
   constructor(private prisma: PrismaService) {}
 
   async category(
     categoryWhereUniqueInput: Prisma.CategoryWhereUniqueInput,
   ): Promise<Category | null> {
+    this.logger.log(
+      `Searching a category with criteria ${JSON.stringify(
+        categoryWhereUniqueInput,
+      )}`,
+    );
+
+    // finding the category using prisma
     return this.prisma.category.findUnique({
       where: categoryWhereUniqueInput,
     });
@@ -23,6 +32,8 @@ export class CategoryService {
   }): Promise<Category[]> {
     const { skip, take, cursor, where, orderBy } = params;
 
+    this.logger.log(`Getting list of categories from ${skip} to ${take}`);
+
     return this.prisma.category.findMany({
       skip,
       take,
@@ -33,6 +44,8 @@ export class CategoryService {
   }
 
   async createCategory(data: Prisma.CategoryCreateInput): Promise<Category> {
+    this.logger.log(`Creating category with data ${JSON.stringify(data)}`);
+    // creating the category
     return this.prisma.category.create({ data });
   }
 
@@ -41,6 +54,7 @@ export class CategoryService {
     data: Prisma.CategoryUpdateInput;
   }): Promise<Category> {
     const { where, data } = params;
+    this.logger.log(`Updating category with data ${JSON.stringify(data)}`);
 
     return this.prisma.category.update({ data, where });
   }
@@ -48,6 +62,7 @@ export class CategoryService {
   async deleteCategory(
     where: Prisma.CategoryWhereUniqueInput,
   ): Promise<Category> {
+    this.logger.log(`Deleting category with criteria ${JSON.stringify(where)}`);
     return this.prisma.category.delete({ where });
   }
 }
